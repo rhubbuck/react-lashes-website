7b8a582f275bf6155454601e604f4312ad9824cc
Ryan Hubbuck <rhubbuck@gmail.com>
2022-02-22 13:50:05 -0500

    emailjs conntected

--- react-lashes-website/src/Contact.js
+++ react-lashes-website/src/Contact.js
@@ -1,58 +1,36 @@
-import React from 'react';
-import { Form, FormControl, FormGroup, Button} from 'react-bootstrap';
-import { useForm, Controller } from 'react-hook-form';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import './Contact.css';
 
 const Contact = () => {
 
-    const { register, handleSubmit, errors } = useForm();
+    const form = useRef();
 
-    const onSubmit = (data) => {
-        console.log(data);  
+    function sendEmail(e) {
+        e.preventDefault();
+
+    emailjs.sendForm('service_ij20fx9', 'template_v8xyku7', form.current, 'user_ViWYiGu4yLdFIXPcCAgcc')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
     }
 
-
   return (
-    <div className='contact-wrapper'>Contact
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" name="name" {...register('name', { required: true })}/>
-        <input type="email" name="email" {...register('email',{ required: true })} />
-        <input type="text" name="message" {...register('message', { required: true })}/>
-        <input type="submit"/>
+    <div className='contact-wrapper'>
+        <h1 className='contact-header'>(123) 123 - 1234</h1>
+        <h2 className='contact-subheader'>Have a question? Contact me here!</h2>
+         <form ref={form} onSubmit={sendEmail} className="contact-form">
+            <label className='contact-label'>Name</label>
+            <input type="text" name="name" required className='contact-input' />
+            <label className='contact-label'>Email</label>
+            <input type="email" name="email" required className='contact-input' />
+            <label className='contact-label'>Message</label>
+            <textarea name="message" required className='contact-textarea' />
+            <input type="submit" value="Send Message" className='contact-submit' />
         </form>
-        {/* <Form className='contact-form' onSubmit={handleSubmit(onSubmit)} >
-            <FormGroup className="mb-3" controlId="formEmail">
-                <Form.Label>Email address</Form.Label>
-                <FormControl type="email" placeholder="Enter email" />
-            </FormGroup>
-
-            <Form.Group className="mb-3" controlId="formPassword">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="First & Last name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formMessage">
-                <Form.Label>Message</Form.Label>
-                <Form.Control type="text" placeholder='Enter message here' />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form> */}
-        {/* <form id="contact-form"  onSubmit={this.submit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5" />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form> */}
     </div>
   )
 }
